265b0b7b249462694d29ce247a0ab62351a4cbc6
chaco <tech.mh18@gmail.com>
2018-05-24 14:15:44 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:show, :create, :update, :destroy]
   
   def index
     if logged_in?
